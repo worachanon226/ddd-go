cebee92a5c2aadb6c3991ac696c78260c49d19d7
PuNe <57552176+worachanon226@users.noreply.github.com>
2023-02-22 13:59:14 +0700

    update structures

--- main.go
+++ main.go
@@ -3,3 +3,35 @@ package main
 func main() {
 
 }
+
+// app/
+// ├─ 📄 main.go
+// 📂 assets/
+// ├─ 📂 logs/
+// 📂 configs/
+// ├─ 📄 configs.go
+// 📂 modules/
+// ├─ 📂 servers/
+// │  ├─ 📄 server.go
+// │  ├─ 📄 handler.go
+// ├─ 📂 entities/
+// │  ├─ 📄 users.go
+// │  ├─ 📄 response.go
+// ├─ 📂 users/
+// │  ├─ 📂 controllers/
+// │  │  ├─ 📄 users_controllers.go
+// │  ├─ 📂 usecases/
+// │  │  ├─ 📄 users_usecases.go
+// │  ├─ 📂 repositories/
+// │  │  ├─ 📄 users_repositories.go
+// 📂 pkg/
+// ├─ 📂 databases/
+// │  ├─ 📂 migrations/
+// │  ├─ 📄 postgresql.go
+// ├─ 📂 middlewares/
+// ├─ 📂 utils/
+// │  ├─📄 connection_url_builder.go
+// 📂 tests/
+// ├─ 📂 users/
+// │  ├─ 📄 users_test.go
+// 📄 .env
