601bd45b05caabb2118ca67890659a82c1abb004
PuNe <57552176+worachanon226@users.noreply.github.com>
2023-02-22 14:57:52 +0700

    update entities

--- modules/entities/users.go
+++ modules/entities/users.go
@@ -1,1 +1,19 @@
 package entities
+
+type UsersUsecase interface {
+	Register(req *UsersResgisterReq) (*UsersRegisterRes, error)
+}
+
+type UserRepository interface {
+	Register(req *UsersResgisterReq) (*UsersRegisterRes, error)
+}
+
+type UsersResgisterReq struct {
+	Username string `json:"username" db:"username"`
+	Password string `json:"password" db:"password"`
+}
+
+type UsersRegisterRes struct {
+	Id       int64  `json:"id" db:"id"`
+	Username string `json:"username" db:"username"`
+}
